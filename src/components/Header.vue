6f53998418b40a5d6bd83610821b965f1d93d9c5
uuuu-8 <104149556+ueno-8-ueno@users.noreply.github.com>
2024-01-09 23:03:28 +0900

    [Update]更新日追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,7 @@
                     Ueno's Portfolio
                 </router-link>
             </v-app-bar-title>
+            <p class="version">updated 2024/1/9</p>
         </v-app-bar>
     </header>
 </template>
@@ -16,5 +17,10 @@
   color: inherit;
   font-size: 30px;
   margin-left: 2%;
+}
+
+.version{
+    text-align: right;
+    padding-right: 2%;
 }
 </style>
